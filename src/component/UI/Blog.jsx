a198ae3ac492f75ded4e32d1c351b6efe76b1dea
milkessa <milkessagebi@gmail.com>
2023-01-16 15:59:22 +0300

    monday

--- src/component/UI/Blog.jsx
+++ src/component/UI/Blog.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "../../styles/blog.css";
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <section>
+      <div className='container'>
+        <div className='blog__top-content'>
+          <h6 className='subtitle'>Our blog</h6>
+          <h2>
+            Let's have a look from our
+            <span className='highlight'>recent blog</span>
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
